314b7afc804d00854bb0412b27bdcbd21ec4c56f
Nils Waldowski <me@nilswaldowski.de>
2017-06-30 19:54:43 +0200

    [TASK] added eslint to webpack setup

--- 01-src/javascript/main.js
+++ 01-src/javascript/main.js
@@ -1,3 +1,6 @@
-import { map } from 'lodash';
+import _ from 'lodash';
+import greet from './modules/greet'
 
-console.log(map([1,2,3,4,5,6], n => n*n));
+console.log(_.map([1,2,3,4,5,6], n => n*n));
+
+greet();
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,26 @@
-
 var path = require('path');
 
 module.exports = {
     entry: './01-src/javascript/main.js',
     output: {
-        //path: __dirname + '/dist',
         path: path.resolve(__dirname, 'dist/javascript'),
         filename: 'bundle.js'
     },
     module: {
         rules: [
             {
+                enforce: 'pre',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader',
+                options: {
+                    emitWarning: true
+                }
+            },
+            {
                 test: /\.jsx?$/,
                 loader: 'babel-loader',
-                exclude: /(node_modules|bower_components)/
+                exclude: /node_modules/
             }
         ]
     }
