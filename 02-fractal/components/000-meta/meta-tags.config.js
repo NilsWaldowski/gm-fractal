5246ae74bdeb23742a39608cf5dff3e103d8b0a5
Nils Waldowski <me@nilswaldowski.de>
2017-07-28 13:05:26 +0200

    [TASK] split _preview parts (meta tags, assets etc.) in different components

---
+++ 02-fractal/components/000-meta/meta-tags.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    title: 'Page Meta Tags',
+    description: '',
+    status: 'wip',
+    context: {
+
+        domain: 'http://gm7-lab.cps-projects.de',
+        description: 'A description of the page',
+        img: {
+            share: '/typo3conf/ext/bc_sitepackage/Resources/Public/_Default/images/logo.png',
+            favicon: {
+                ico: '/typo3conf/ext/bc_sitepackage/Resources/Public/_Default/images/favicon/favicon.ico',
+                s16: '/typo3conf/ext/bc_sitepackage/Resources/Public/_Default/images/favicon/favicon-16.png',
+                s32: '/typo3conf/ext/bc_sitepackage/Resources/Public/_Default/images/favicon/favicon-32.png',
+                s48: '/typo3conf/ext/bc_sitepackage/Resources/Public/_Default/images/favicon/favicon-48.png',
+                s62: '/typo3conf/ext/bc_sitepackage/Resources/Public/_Default/images/favicon/favicon-62.png',
+                s192: '/typo3conf/ext/bc_sitepackage/Resources/Public/_Default/images/favicon/favicon-192.png'
+            },
+            apple: {
+                touch: '/typo3conf/ext/bc_sitepackage/Resources/Public/_Default/images/favicon/favicon-16.png'
+            }
+        }
+    }
+}
