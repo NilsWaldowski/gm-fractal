c1dabd72e9108b3053a8422cfd42ae2faac3b8a6
Nils Waldowski <me@nilswaldowski.de>
2017-07-28 19:56:19 +0200

    [TASK] removed unnecessary ";"

--- fractal.js
+++ fractal.js
@@ -1,47 +1,47 @@
 /* Create a new Fractal instance and export it for use elsewhere if required */
-const fractal = module.exports = require('@frctl/fractal').create();
+const fractal = module.exports = require('@frctl/fractal').create()
 
 // set logger const
-const logger = fractal.cli.console; // keep a reference to the fractal CLI console utility
+const logger = fractal.cli.console // keep a reference to the fractal CLI console utility
 
 /* Set the title of the project */
-fractal.set('project.title', 'FooCorp Component Library');
+fractal.set('project.title', 'FooCorp Component Library')
 
 /* Tell Fractal where the components will live */
-fractal.components.set('path', __dirname + '/02-fractal/components');
+fractal.components.set('path', __dirname + '/02-fractal/components')
 
 /* Tell Fractal where the documentation pages will live */
-fractal.docs.set('path', __dirname + '/02-fractal/docs');
+fractal.docs.set('path', __dirname + '/02-fractal/docs')
 
 // Project config
-fractal.set('project.title', 'CPS-IT GoldenMaster');
+fractal.set('project.title', 'CPS-IT GoldenMaster')
 
 // Components config
-const engineInstance = fractal.components.engine();
+const engineInstance = fractal.components.engine()
 
 // Using handlebars-layouts (https://www.npmjs.com/package/handlebars-layouts)
-const layouts = require('handlebars-layouts');
-layouts.register(engineInstance.handlebars);
+const layouts = require('handlebars-layouts')
+layouts.register(engineInstance.handlebars)
 
-fractal.components.set('default.preview', '@preview');
+fractal.components.set('default.preview', '@preview')
 
 // Build - will be deployed by jenkins or manually for public project visibility
-fractal.web.set('builder.dest', __dirname + '/public');
+fractal.web.set('builder.dest', __dirname + '/public')
 
 /*
 * Assets
 */
-fractal.web.set('static.path', __dirname + '/dest');
+fractal.web.set('static.path', __dirname + '/dest')
 // match /dest path with deploy environment in TYPO3
-fractal.web.set('static.mount', '/typo3conf/ext/gm8_sitepackage/Resources/Public/_Default/Frontend');
+fractal.web.set('static.mount', '/typo3conf/ext/gm8_sitepackage/Resources/Public/_Default/Frontend')
 
 fractal.web.set('server.syncOptions', {
     open: true,
     //browser: ['google chrome', 'firefox'],
     notify: true
-});
+})
 
 module.exports = {
     fractal: fractal,
     logger: logger
-};
+}
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp')
 const path = require('path')
 const requireDir = require('require-dir')
 
-const f = require('./fractal.js');
+const f = require('./fractal.js')
 
 // Fallback for windows backs out of node_modules folder to root of project
 process.env.PWD = process.env.PWD || path.resolve(process.cwd(), '../../')
@@ -23,24 +23,24 @@ requireDir('./03-internal/gulp', { recurse: false })
 gulp.task('fractal:start', function(){
     const server = f.fractal.web.server({
         sync: true
-    });
-    server.on('error', err => f.logger.error(err.message));
+    })
+    server.on('error', err => f.logger.error(err.message))
     return server.start().then(() => {
-        f.logger.success(`Fractal server is now running at ${server.url}`);
-    });
-});
+        f.logger.success(`Fractal server is now running at ${server.url}`)
+    })
+})
 
 /**
 * Fractal Build
 */
 gulp.task('fractal:build', function(){
-    const builder = f.fractal.web.builder();
-    builder.on('progress', (completed, total) => f.logger.update(`Exported ${completed} of ${total} items`, 'info'));
-    builder.on('error', err => f.logger.error(err.message));
+    const builder = f.fractal.web.builder()
+    builder.on('progress', (completed, total) => f.logger.update(`Exported ${completed} of ${total} items`, 'info'))
+    builder.on('error', err => f.logger.error(err.message))
     return builder.build().then(() => {
-        f.logger.success('Fractal build completed!');
-    });
-});
+        f.logger.success('Fractal build completed!')
+    })
+})
 
 /**
 * Init Assets
@@ -56,7 +56,7 @@ gulp.task('init', gulp.series('clean',
         'misc',
         'fonts'
     )
-));
+))
 
 
 /**
@@ -65,19 +65,19 @@ gulp.task('init', gulp.series('clean',
 gulp.task('build', gulp.series(function(done) {
     global.production = true
     done()
-}, 'init'));
+}, 'init'))
 
 /**
 * Watchs Assets & Start Fractal Browser-Sync
 */
-gulp.task('watch-and-serve', gulp.parallel('watch', 'fractal:start'));
+gulp.task('watch-and-serve', gulp.parallel('watch', 'fractal:start'))
 
 /**
 * Init Assets and start Watcher & Fractal Browser-Sync afterwards
 */
-gulp.task('default', gulp.series('init', 'watch-and-serve'));
+gulp.task('default', gulp.series('init', 'watch-and-serve'))
 
 /**
 * Deploy
 */
-gulp.task('deploy', gulp.series('build', 'copyBuild'));
+gulp.task('deploy', gulp.series('build', 'copyBuild'))
