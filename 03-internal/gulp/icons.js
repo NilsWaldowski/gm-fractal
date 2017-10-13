c7b414a57cae1da4b60db591c673c230d6440782
Nils Waldowski <me@nilswaldowski.de>
2017-10-13 12:02:20 +0200

    [TASK] update gulp tasks and npm packages (prepared better icon/svg handling through svgson)

--- 03-internal/gulp/icons.js
+++ 03-internal/gulp/icons.js
@@ -3,9 +3,10 @@ const gulp      = require('gulp')
 const imagemin  = require('gulp-imagemin')
 const svgSprite = require('gulp-svg-sprite')
 const path      = require('path')
-const fancyLog      = require('fancy-log')
-const chalk         = require('chalk')
-const emoji         = require('node-emoji')
+const fancyLog  = require('fancy-log')
+const chalk     = require('chalk')
+const emoji     = require('node-emoji')
+const npmRun    = require('npm-run')
 
 const iconsTask = function() {
 
@@ -18,12 +19,22 @@ const iconsTask = function() {
     fancyLog(chalk.bgGreen.white.bold(' ' + emoji.get('heart') + '  ' +
         'Optimize Icons & create Sprite' + ' ' + emoji.get('heart') + '  '))
 
+    /*
+    @TODO: try to implement SVGSON directy to the icon task!
+    npmRun.exec('svgson -i ../../01-src/icons -t -p', function (err, stdout, stderr) {
+        // err Error or null if there was no error
+        // stdout Buffer|String
+        // stderr Buffer|String
+        console.log(stdout);
+    })
+    */
+
     return gulp.src([paths.src, '*!README.md'])
-        .pipe(changed(paths.dest)) // Ignore unchanged files
+        //.pipe(changed(paths.dest)) // Ignore unchanged files
         .pipe(imagemin())
         .pipe(gulp.dest(paths.dest))
         .pipe(svgSprite(TASK_CONFIG.icons.settings))
-        .pipe(gulp.dest(paths.dest + '/sprite'))
+        .pipe(gulp.dest(paths.dest))
 }
 
 gulp.task('icons', iconsTask)
